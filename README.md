fc50aa9e6b645dfe38adf3a87bacdd30346a89d5
robincpc <chienpinchen@gmail.com>
2019-11-10 02:08:47 -0800

    Add first draft for mqtt_monitor package

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # mqtt_monitor
-A ROS package for Nexcom SRB to send the robot arm information to AWS cloud through mqtt.
+A ROS package for NexCOBOT Smart Robot Box (SRB) to send the robot arm information to AWS cloud through mqtt.
+
+## Dependence
+* NexCOBOT SRB robot_gateway
+* [AWS IoT Bridge](https://github.com/aws-robotics/aws-iot-bridge-example) (Cloud extension for ROS)
+* [mqtt_bridge](https://github.com/RobinCPC/mqtt_bridge) (note:I made some modified in my fork)
+
+## Run
+### Start UR robot and NexCOBOT SRB robot_gateway
+``` bash
+# In terminal, use bash as root (because modbus connection use port 502 which need root permission)
+sudo bash
+roslaunch robot_gateway SRB.launch
+# Open new terminal, connect robot gateway with UR robot
+rosservice call /TrfConnectInfo "vender: 'UR' ip: '192.168.0.200' port: 1"  # note: the ip will need to find from UR controller
+```
+
+### Start mqtt_ur_monitor
+``` bash
+roslaunch mqtt_monitor mqtt_ur_monitor.launch
+```
+
+## Architecture
+![architecture](./docs/images/UR_Monitor_AWS.png)
+
---
+++ include/mqtt_monitor/mqtt_ur_monitor.h
@@ -0,0 +1,50 @@
+// Author: Robin Chen
+#ifndef  MQTT_UR_MONITOR_H
+#define  MQTT_UR_MONITOR_H
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <sensor_msgs/JointState.h>
+#include <robot_gateway/ur_msg.h>
+
+#include <vector>
+#include <string>
+
+#define DEG2RAD 0.017453292519943
+
+class MqttUrMonitor
+{
+public:
+  MqttUrMonitor(ros::NodeHandle nh);  // Constructor
+  ~MqttUrMonitor();                   // Destructor
+
+private:
+  ros::NodeHandle nh_;
+
+  ros::Subscriber ur_mod_sub;
+  ros::Publisher ur_jnt_pub;
+  ros::Publisher ur_mqtt_pub;
+  ros::Subscriber ur_mqin_sub;
+
+  //ros::Publisher time_pub;
+  ros::Time start_time;
+  ros::Time update_time;
+
+  std::vector<std::string> jointNames = {"shoulder_pan_joint",
+                                         "shoulder_lift_joint",
+                                         "elbow_joint",
+                                         "wrist_1_joint",
+                                         "wrist_2_joint",
+                                         "wrist_3_joint"};
+  const double r2d = 57.295779513;
+  const double d2r = 0.017453292519943;
+  bool in_cloud = false;
+
+  void ur_msg_callback(const robot_gateway::ur_msg& ur_mod_msg);
+  void ur_mqin_callback(const std_msgs::String& ur_mqin_msg);
+  void pub_joint_state(const std::vector<double>& jntVals);
+  std::vector<std::string> split(std::string cmd, std::string delimiter);
+
+};
+#endif // MQTT_UR_MONITOR_H
+
