64f43bac495d3bb04f12c47a63bb84a21d741f0a
robincpc <chienpinchen@gmail.com>
2020-01-04 09:31:00 +0000

    modify yk_monitor to send joint array for moveit joint position command and modify launch file to update rosparam by robomaker ENV variable

--- include/mqtt_monitor/mqtt_yk_monitor.h
+++ include/mqtt_monitor/mqtt_yk_monitor.h
@@ -4,6 +4,8 @@
 
 #include <ros/ros.h>
 #include <std_msgs/String.h>
+#include <std_msgs/Float64MultiArray.h>
+#include <std_msgs/MultiArrayDimension.h>
 #include <sensor_msgs/JointState.h>
 
 #include <vector>
@@ -22,10 +24,13 @@ private:
 
   ros::Publisher yk_jnt_pub;
   ros::Subscriber yk_mqin_sub;
+  ros::Publisher target_jnt_pub;
+  std_msgs::Float64MultiArray target_jnt_msg;
 
   ros::Time start_time;
   ros::Time update_time;
 
+  // TODO: use config yaml to load jointNames
   std::vector<std::string> jointNames = {"joint_1_s",
                                          "joint_2_l",
                                          "joint_3_u",
@@ -36,7 +41,9 @@ private:
   const double r2d = 57.295779513;
   const double d2r = 0.017453292519943;
   const double mm2meter = 0.001;
-  bool in_cloud = false;
+  bool in_cloud = false;  // check if run in the cloud service
+  bool is_sim = false;    // check if use Gazebo for simulation
+  double sampling_time = 0.1;  // time period for message publish to mqtt or control interface
 
   void yk_mqin_callback(const std_msgs::String& yk_mqin_msg);
   void pub_joint_state(const std::vector<double>& jntVals);
--- src/mqtt_yk_monitor.cpp
+++ src/mqtt_yk_monitor.cpp
@@ -7,11 +7,26 @@ MqttYkMonitor::MqttYkMonitor(ros::NodeHandle nh)
   start_time = ros::Time::now();
   update_time = start_time;
 
-  nh_.param<bool>("is_sim", in_cloud, false);
-  ROS_INFO_STREAM("Is This Node in AWS Cloud? " << in_cloud);
+  nh_.param<bool>("in_cloud", in_cloud, true);
+  ROS_INFO_STREAM("Is mqtt_monitor Node in AWS Cloud? " << in_cloud);
+  nh_.param<bool>("is_sim", is_sim, false);
+  ROS_INFO_STREAM("Is mqtt_monitor Node do simulation with Gazebo? " << is_sim);
+  nh_.param<double>("sampling_time", sampling_time, 0.1);
+  ROS_INFO_STREAM("Sampling time (sec) for sending out message: " << sampling_time);
 
-  // Publish Yaskawa joints value to joint_states for rviz.
-  yk_jnt_pub = nh_.advertise<sensor_msgs::JointState>("/yk/joint_states", 1);
+  if(is_sim)
+  {
+    target_jnt_pub = nh_.advertise<std_msgs::Float64MultiArray>("/yk/target_jnt_data", 1);
+
+    this->target_jnt_msg.layout.dim.push_back(std_msgs::MultiArrayDimension());
+    this->target_jnt_msg.layout.dim[0].size = 6;  // TODO: use rosparam to get correct value.
+    this->target_jnt_msg.layout.dim[0].stride = 1;
+    this->target_jnt_msg.layout.dim[0].label = "Joint"; 
+  }else
+  {
+    // Publish Yaskawa joints value to joint_states for rviz.
+    yk_jnt_pub = nh_.advertise<sensor_msgs::JointState>("/yk/joint_states", 1);
+  }
   // Subscribe YasKawa data from AWS IoT MQTT Bridge
   yk_mqin_sub = nh_.subscribe("/mqin_ur_data", 1, &MqttYkMonitor::yk_mqin_callback, this);
 
@@ -26,17 +41,13 @@ void MqttYkMonitor::yk_mqin_callback(const std_msgs::String& yk_mqin_msg)
   jnt_str = split(yk_mqin_msg.data, ",");
 
   std::vector<double> jointVals = {};
-  //for(size_t i=1; i < 5; ++i) 	// TODO: update joint number by rosparam, not fixed.
-  //{
-  //  jointVals.push_back( std::stod(jnt_str[i]) * d2r);
-  //}
-  jointVals.push_back( std::stod(jnt_str[1]) * d2r);
-  jointVals.push_back( std::stod(jnt_str[2]) * d2r);
-  jointVals.push_back( std::stod(jnt_str[3]) * d2r);
-  jointVals.push_back( std::stod(jnt_str[4]) * d2r);
-  jointVals.push_back( std::stod(jnt_str[5]) * d2r);
-  jointVals.push_back( std::stod(jnt_str[6]) * d2r);
-  pub_joint_state(jointVals);
+  // TODO: update joint number by rosparam, not fixed.
+  for(size_t i=1; i < 7; ++i)
+  {
+    jointVals.push_back( std::stod(jnt_str[i]) * d2r);
+  }
+  if(!is_sim)
+    pub_joint_state(jointVals);
 
   std::string tmp_str ="";
   for (auto& s : jnt_str)
@@ -44,6 +55,20 @@ void MqttYkMonitor::yk_mqin_callback(const std_msgs::String& yk_mqin_msg)
     tmp_str += s + " ";
   }
   ROS_DEBUG_STREAM("MQTT in message: " << tmp_str);
+  
+  // Publish target joint to the control interface
+  ros::Duration time_diff = (ros::Time::now() - update_time);
+  if (is_sim && time_diff.toSec() >= sampling_time)
+  {
+    update_time = ros::Time::now();
+    //ros::Duration power_on_time = update_time - start_time;
+
+    // publish target joint value
+    this->target_jnt_msg.data.clear();
+    this->target_jnt_msg.data.insert(this->target_jnt_msg.data.end(),
+        jointVals.begin(), jointVals.end());
+    this->target_jnt_pub.publish(this->target_jnt_msg);
+  } 
 
   return;
 }
